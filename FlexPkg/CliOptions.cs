4bacd365aecae1ac8e6d782702b8b91ee7978f44
enchart <77974051+enchart@users.noreply.github.com>
2024-08-06 01:28:29 +0600

    Change default config.json folder to userdata

--- FlexPkg/CliOptions.cs
+++ FlexPkg/CliOptions.cs
@@ -4,6 +4,6 @@ namespace FlexPkg;
 
 public class CliOptions
 {
-    [Option('c', "config", HelpText = "Path to the config file.", Default = "config.json")]
-    public string ConfigPath { get; set; } = "config.json";
+    [Option('c', "config", HelpText = "Path to the config file.", Default = "userdata/config.json")]
+    public string ConfigPath { get; set; } = "userdata/config.json";
 }
